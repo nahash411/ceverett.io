b25e3e42563737eb288e0e6a560a278218b8c3de
Christian Everett <christian.everett@hengedocks.com>
2018-03-12 16:19:01 -0700

    Removed auth0 API keys from code

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'ceverett.auth0.com',
-  clientId: 'CX1JZUEsOoyUay6VoNP719HgSoU6OtOi',
-  callbackUrl: 'http://localhost:3000/callback'
+  clientId: process.env.AUTH_ID,
+  callbackUrl: process.env.CALLBACK
 }

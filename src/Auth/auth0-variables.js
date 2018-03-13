4896e444bf87940952cb2a2b39fb0e404a1a5150
Christian Everett <christian.everett@hengedocks.com>
2018-03-13 09:38:47 -0700

    Added dotenv config variables

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'ceverett.auth0.com',
-  clientId: process.env.AUTH_ID,
-  callbackUrl: process.env.CALLBACK
+  clientId: process.env.REACT_APP_AUTH_ID,
+  callbackUrl: process.env.REACT_APP_CALLBACK
 }

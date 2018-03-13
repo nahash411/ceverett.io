9eb53392b06b307eb76e08055faff4c7bd552b2c
Christian Everett <christian.everett@hengedocks.com>
2018-03-13 09:46:18 -0700

    Changed navbar branding from Auth0 boilerplate to ceverett.io

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
         <Navbar fluid>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#">Auth0 - React</a>
+              <h3>ceverett.io</h3>
             </Navbar.Brand>
             <Button
               bsStyle="primary"

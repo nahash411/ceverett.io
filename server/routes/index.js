c502a96935c7df9888ec8088840076f01ef5a27f
Christian Everett <christian.everett@hengedocks.com>
2018-04-03 10:44:40 -0700

    Added categories route to API

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
+const redis = require("redis")
+const client = redis.createClient()
+
 const async_middleware = require('../utils/middleware');
 
 router.get('/', async_middleware(async (req, res, next) => {
   res.json({ 'title': 'Found the API' })
+}))
+
+router.get('/categories', async_middleware(async (req, res, next) => {
+
+  client.smembers('categories', (err, categories) => {
+    res.json(categories)
+  })
+
 }))
 
 module.exports = router

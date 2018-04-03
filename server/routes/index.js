dbdc3756ab01dd7ae1bd7638f8de0030e199adc6
Christian Everett <christian.everett@hengedocks.com>
2018-04-02 17:08:03 -0700

    Added async/await  middleware for routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ 'title': 'Found the API' });
-});
+const async_middleware = require('../utils/middleware');
 
-module.exports = router;
+router.get('/', async_middleware(async (req, res, next) => {
+  res.json({ 'title': 'Found the API' })
+}))
+
+module.exports = router
